1a88bb08a37e817e115034a2022859312529ba84
MARC JUAREN GAMILLA <124160084+Emjiii@users.noreply.github.com>
2024-11-11 23:16:04 +0800

    added sample logic gates: and, or, and not

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,34 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
 
-app.get("/api", (req, res) =>{
-    res.json({"users": ["userOne", "userTwo", "userThree", "userFour", "testing...", "try"]})
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//Logic Gate Endpoints
+app.post('/api/and', (req, res) => {
+    const {input1, input2} = req.body;
+    const output = input1 && input2;
+    res.json({output});
+});
+
+app.post('/api/or', (req, res) => {
+    const {input1, input2} = req.body;
+    const output = input1 || input2;
+    res.json({output});
 })
 
-app.listen(5000, (console.log("Server started on port 5000")))
+app.post('/api/not', (req, res) => {
+    const {input} = req.body;
+    const output = !input;
+    res.json({output}); 
+});
+
+
+
+// app.get("/api", (req, res) =>{
+//     res.json({"users": ["userOne", "userTwo", "userThree", "userFour", "testing...", "try"]})
+// })
+
+app.listen (PORT, () => {
+    console.log('Server is running on http://localhost:${PORT}');
+});
