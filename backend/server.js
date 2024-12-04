22b3534305487f54061982253aa279fa29950112
MARC JUAREN GAMILLA <124160084+Emjiii@users.noreply.github.com>
2024-12-04 22:09:45 +0800

    hsting

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const gateRoutes = require('./src/gates/gateRoutes');
 const truthTableRoutes = require('./src/truthTable/truthTableRoutes');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
