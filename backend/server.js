a8f55d2ea88cecadbf4ec5a7b0196519039894bd
MARC JUAREN GAMILLA <124160084+Emjiii@users.noreply.github.com>
2024-12-07 02:17:46 +0800

    fix routing

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,6 @@ app.get('/', (req, res) => {
 app.use('/gates', gateRoutes);
 app.use('/truth-table', truthTableRoutes);
 
-app.post('/truth-table/circuit', truthTableRoutes);
 
 // Endpoint to get the current state of a power switch
 app.get('/api/power-switch/:id', (req, res) => {
